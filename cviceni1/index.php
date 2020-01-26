4b7b0700484133576fbf98ebe33088e87763085e
Milan Novák <novak55@vspj.cz>
2020-01-26 22:04:13 +0100

    Osobni stranka init

--- cviceni1/index.php
+++ cviceni1/index.php
@@ -5,13 +5,54 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<meta name="HandheldFriendly" content="True">
 	<meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="../js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<title>
-		Rozcestník na projekty
+        Moje osobní stránka
 	</title>
 </head>
-<body>
-<h1>Rozcestník na projekty</h1>
-<ul>
-	<li><a href="#"></a>Moje osobní stránka</li>
-</ul>
+<body class="bg-light">
+    <div class="jumbotron justify-content-md-around text-center">
+        <h1>Moje osobní stránka</h1>
+        <a class="btn btn-info" href="../index.php">Zpět na rozcestník</a>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="offset-1">
+                <h2>O mě</h2>
+                <p>
+                    Ahoj, jmenuji se Milan Novák.<br>
+                    Pracuji na Vysoké škole polytechnické Jihlava<br>
+                    Jsem z Jihlavy.
+                </p>
+                <h3>Záliby</h3>
+                <ul style="font-family: Arial">
+                    <li><strong>Sporty - aktivně</strong>
+                        <ul>
+                            <li>hokej,</li>
+                            <li>bedbinton.</li>
+                        </ul>
+                    </li>
+                    <li><strong>Koníčky</strong>
+                        <ul>
+                            <li>jízda na motorce,</li>
+                            <li>rodina.</li>
+                        </ul>
+                    </li>
+                </ul>
+                <h3>Kontakt</h3>
+                email: <a href="mailto:milan.novak@xxxxxx.xxxxx">milan.novak@xxxxx.xxxxx</a><br>
+                telefon: <a href="tel:+420123456789">123 456 789</a>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="offset-1">
+                <h2>Moje bydliště - Jihlava</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row bg-dark">
+        <div class="col-sm-12 text-success text-right">Datum vytvoření: 26. 1. 2020  </div>
+    </div>
+
 </body>
--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<meta name="HandheldFriendly" content="True">
 	<meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<title>
 		Rozcestník na projekty
 	</title>
 </head>
 <body>
-<h1>Rozcestník na projekty</h1>
-<ul>
-	<li><a href="cviceni1/index.php"></a>Moje osobní stránka</li>
-</ul>
+    <div class="jumbotron justify-content-md-around text-center">
+        <h1>Rozcestník na projekty</h1>
+    </div>
+    <ul class="col-sm-10 offset-sm-1 list-group">
+        <li class="list-group-item"><a href="cviceni1/index.php">Cvičení 1 - Moje osobní stránka</a></li>
+    </ul>
 </body>
