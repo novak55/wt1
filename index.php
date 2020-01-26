8b502d5d685fbbab3d511121d9f1779d71d6b299
Milan Novák <novak55@vspj.cz>
2020-01-26 19:51:26 +0100

    wt1 init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=9" />
+	<meta name="HandheldFriendly" content="True">
+	<meta name="viewport" content="width=device-width">
+	<title>
+		Rozcestník na projekty
+	</title>
+</head>
+<body>
+<h1>Rozcestník na projekty</h1>
+<ul>
+	<li><a href="#"></a>Moje osobní stránka</li>
+</ul>
+</body>
