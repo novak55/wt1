c522399302c190007ecb134189d6823c8a8d0c71
Milan Novák <novak55@vspj.cz>
2020-01-27 00:36:14 +0100

    Finish

--- cviceni1/index.php
+++ cviceni1/index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="cs">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=9" />
 	<meta name="HandheldFriendly" content="True">
 	<meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="../js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <!--<script src="../js/bootstrap.min.js" crossorigin="anonymous"></script>-->
+    <title>
         Moje osobní stránka
 	</title>
 </head>
@@ -43,16 +46,88 @@
                 <h3>Kontakt</h3>
                 email: <a href="mailto:milan.novak@xxxxxx.xxxxx">milan.novak@xxxxx.xxxxx</a><br>
                 telefon: <a href="tel:+420123456789">123 456 789</a>
+                <h3>Obrázky</h3>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="obr/Jihlava_1.jpg" alt="Jihlava - pohled na radnici">
+                            <div class="carousel-caption d-none d-md-block">
+                                <p class="bg-dark">Autor: <a href="//commons.wikimedia.org/wiki/User:SchiDD" title="User:SchiDD">SchiDD</a> – <span class="int-own-work" lang="cs">Vlastní dílo</span>, <a href="https://creativecommons.org/licenses/by-sa/4.0" title="Creative Commons Attribution-Share Alike 4.0">CC BY-SA 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=36398568">Odkaz</a></p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="obr/Jihlava_2.jpg" alt="Jihlava - znak města">
+                            <div class="carousel-caption d-none d-md-block">
+                                <p class="bg-dark"> Autor: <span lang="cs">neznámý</span> – Uploaded from <a href="https://en.wikipedia.org/wiki/cs:Soubor:Jihlava_prapor.gif" class="extiw" title="w:cs:Soubor:Jihlava prapor.gif">Czech Wikipedia</a>, Volné dílo, <a href="https://commons.wikimedia.org/w/index.php?curid=1472404">Odkaz</a></p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="obr/Jihlava_3.jpg" alt="Jihlava - pohled z města na bránu v ulici Matky boží"><div class="carousel-caption d-none d-md-block">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <p class="bg-dark">Autor: scanned by <a href="https://cs.wikipedia.org/wiki/User:Pernak" class="extiw" title="cs:User:Pernak">cs:User:Pernak</a> – 1899 picture-postcard, Volné dílo, <a href="https://commons.wikimedia.org/w/index.php?curid=1642060">Odkaz</a></p>                            </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="col-sm-6">
             <div class="offset-1">
                 <h2>Moje bydliště - Jihlava</h2>
+                <h3>Zajímavosti</h3>
+                Oficiální stránky města Jihlavy: <a href="https://www.jihlava.cz" title="Oficiální stránky města Jihlavy" target="_blank">www.jihlava.cz</a>
+                <p>
+                    <table class="table table-striped col-sm-11">
+                        <thead>
+                        <tr><td colspan="2" class="text-center bg-info"><H4>Informace o městě</H4></td></tr>
+                        </thead>
+                        <tr>
+                            <td>Kraj</td>
+                            <td>Vysočina</td>
+                        </tr>
+                        <tr>
+                            <td>PSČ</td>
+                            <td>58601</td>
+                        </tr>
+                        <tr>
+                            <td>Katastrální výměra</td>
+                            <td>78,85 km²</td>
+                        </tr>
+                        <tr>
+                            <td>Počet obyvatel</td>
+                            <td>50 845</td>
+                        </tr>
+                    </table>
+                </p>
+                <h4>Památky</h4>
+                <ul>
+                    <li><a href="https://pamatkovykatalog.cz/uskp/podle-relevance/1/seznam?uskp=68407%2F7-4884" target="_blank">Sousoší Kalvarie</a></li>
+                    <li><a href="https://pamatkovykatalog.cz/uskp/podle-relevance/1/seznam/?uskp=31839%2F7-4879" target="_blank">Kostel svatého Jana Křtitele</a></li>
+                    <li><a href="https://pamatkovykatalog.cz/uskp/podle-relevance/1/seznam/?uskp=85893%2F7-4892" target="_blank">Socha svatého Jana Nepomuckého </a></li>
+                    <li><a href="https://pamatkovykatalog.cz/uskp/podle-relevance/1/seznam/?uskp=16017%2F7-4918" target="_blank">Smírčí kámen</a></li>
+                    <li><a href="https://pamatkovykatalog.cz/uskp/podle-relevance/1/seznam/?uskp=85946%2F7-4894" target="_blank">Sloup se sochou Panny Marie</a></li>
+                </ul>
+                <h4>Zajímavé místo</h4>
+                <a href="https://www.informuji.cz/objekt/2938-amfiteatr-letni-kino-jihlava/" target="_blank" title="Amfiteátr (letní kino) Jihlava">
+                    <img src="obr/Letni_kino.jpg" alt="Amfiteátr (letní kino) Jihlava" class="col-sm-11">
+                </a>
             </div>
         </div>
     </div>
     <div class="row bg-dark">
         <div class="col-sm-12 text-success text-right">Datum vytvoření: 26. 1. 2020  </div>
     </div>
+<script>
+    $('.carousel').carousel()
+</script>
 
 </body>
+</html>
